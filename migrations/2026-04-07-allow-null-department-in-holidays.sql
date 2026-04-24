740e23fe1c4b1ba6c38af75ea217405d507cb050
PaulBertLandicho <paulbertlandicho.202200536@gmail.com>
2026-04-24 10:49:01 +0800

    27th Commit

--- migrations/2026-04-07-allow-null-department-in-holidays.sql
+++
@@ -1,2 +0,0 @@
--- Migration: Allow department to be NULL for global holidays
-ALTER TABLE public.holidays ALTER COLUMN department DROP NOT NULL;
--- migrations/2026-04-24-create-payroll-periods.sql
+++ migrations/2026-04-24-create-payroll-periods.sql
@@ -1,17 +1,3 @@
--- Migration: add unique constraint/index to prevent duplicate payroll_periods per person+period
-DO $$
-BEGIN
-  IF NOT EXISTS (
-    SELECT 1 FROM pg_class c
-    JOIN pg_namespace n ON n.oid = c.relnamespace
-    WHERE c.relname = 'idx_payroll_person_period' AND n.nspname = 'public'
-  ) THEN
-    CREATE UNIQUE INDEX idx_payroll_person_period ON public.payroll_periods (person_id, period);
-  END IF;
-END
-$$;
-
-
 -- Migration: create payroll_periods table with uniqueness on (person_id, period)
 -- Creates uuid extension if missing and the payroll_periods table.
 
